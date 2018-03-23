3607e961f587c0fa8e53f454dbda4ed419bf60de
Matthew Lourenco <matthew.lourenco@ocsbstudent.ca>
2018-03-23 16:14:23 +0000

    added all code to one document

--- main.swift
+++ main.swift
@@ -6,10 +6,25 @@
 //  Copyright © 2018 MTHS. All rights reserved.
 //
 
+class MyStack {
+	//this is a class that defines a stack
+	private var list: [Int] = []
+
+	public func push(integer: Int) {
+		//append an integer to the end of the stack
+		list.append(integer);
+	}
+	
+	public func getAt(index: Int) -> Int{
+		//This method returns the item at the given index
+		return list[index];
+	}
+}
+
 print("enter an integer")
 let input: String? = readLine(strippingNewline: true)
 if Int(input!) != nil {
-	var stackObject = MyStack()
+	let stackObject = MyStack()
 	stackObject.push(integer: Int(input!)!)
 	print("\(stackObject.getAt(index: 0))")
 }
